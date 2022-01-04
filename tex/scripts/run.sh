570c1d1ee2a6e4b9cc272d535150f84ae0f4e897
Denys Potapov <denys.potapov@gmail.com>
2022-01-04 18:55:54 +0200

    move scripts inside folder

--- tex/scripts/run.sh
+++ tex/scripts/run.sh
@@ -4,7 +4,4 @@ set -e
 DIR=`dirname "$1"`
 TEX=`basename "$1"`
 
-COMMIT_ID=$(git rev-parse --short HEAD)
-echo $COMMIT_ID > 'common/version.tex'
-
 docker exec -it latex_daemon lualatex --interaction=nonstopmode --output-directory=$DIR $TEX
