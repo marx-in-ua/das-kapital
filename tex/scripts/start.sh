f43f64e774522314b28271f9cb9ae0f2ca5b5132
Denys Potapov <denys.potapov@gmail.com>
2023-02-06 23:01:22 +0200

    change docker image

--- tex/scripts/start.sh
+++ tex/scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-IMAGE=blang/latex:ctanfull
-exec docker run -d --rm --name latex_daemon -i --user="$(id -u):$(id -g)" --net=none -t -v $PWD:/data "$IMAGE" /bin/sh -c "sleep infinity"
+IMAGE=texlive/texlive:latest
+exec docker run -d --rm --name latex_daemon -i --net=none -t -v $PWD:/usr/src/app -w /usr/src/app "$IMAGE" /bin/sh -c "sleep infinity"
